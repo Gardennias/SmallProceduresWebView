35b9a570571be3c1eac92f3ae70ab6afc2c13035
tsm <tsm@qq.com>
2019-08-09 17:48:10 +0800

    init

---
+++ pages/component/button/button.vue
@@ -0,0 +1,22 @@
+<template>
+	<view style="text-align: center;margin-top: 60upx;">
+		成功从webVie网页跳转至小程序
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<button type="primary" @click="go_webView">跳转webview</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '小程序页面'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			go_webView() {
+				uni.navigateTo({
+					url: '/pages/web-view/web-view'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
